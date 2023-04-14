8df8bdb94399cc2f2dca3b906bc6478e79d88b84
Adam Dobson-White <adam@onetwo.com.au>
2023-04-14 15:30:47 +1000

    stamp duty works for vic now!

--- myScript.js
+++ myScript.js
@@ -6,40 +6,15 @@ console.log("Hello World")
 
 // ****************
 
-//  Function to have a dropdown for the different states in Australia. This will be used to drive stamp duty and title transfer calculations
-
-function createDropdown(options, dropdownId) {
-    const dropdown = document.createElement("select");
-    dropdown.id = dropdownId;
-
-    options.forEach((option) => {
-        const optionElement = document.createElement("option");
-        optionElement.value = option;
-        optionElement.text = option;
-        dropdown.appendChild(optionElement);
-    });
-
-    return dropdown;
-}
-
-const states = ["", "New South Wales", "Victoria", "Queensland", "Western Australia", "South Australia", "Tasmania", "Australian Capital Territory", "Northern Territory"];
-
-const dropdownContainer = document.getElementById("stateDropdown");
-const stateDropdown = createDropdown(states, "state-dropdown");
-dropdownContainer.appendChild(stateDropdown);
-
-
-// ****************
-
 //  This function calculates the stamp duty based on the state and property value
 function stampDutyCalculator() {
     console.log('called stampDutyCalculator function')
 
     // Get the State of the property
-    const stateDropdown = document.getElementById("stateDropdown").children;
-        console.log(stateDropdown);   
+    const stateDropdown = document.getElementById("stateDropdown");
+    console.log(stateDropdown);
     const state = stateDropdown.value;
-        console.log(state);
+    console.log(state);
 
     // Get the Property Price amount
     const propertyPriceElement = document.getElementById("propertyPrice");
@@ -48,58 +23,59 @@ function stampDutyCalculator() {
 
     //  Start with Stamp Duty at 0
     let stampDuty = 0;
+    let stampDutyCharge = 0
+    let stampDutyRate = 0;
 
     // Perform Stamp Duty calculation for each state
     switch (state) {
-        case "New South Wales":
-            // taxRate = 0.10;
+        case "ACT":
             stampDuty = propertyPrice + 1
-            // total = subtotal + (subtotal * taxRate);
             break;
-        case "Queensland":
-            // taxRate = 0.08;
-            stampDuty = 120; // Queensland has an extra $20 surcharge
-            // total = subtotal + (subtotal * taxRate);
+        case "NSW":
+            stampDuty = propertyPrice + 2
             break;
-        case "South Australia":
-            // taxRate = 0.09;
-            // total = subtotal + (subtotal * taxRate) + 10; // SA has a $10 handling fee
+        case "NT":
+            stampDuty = propertyPrice + 3
             break;
-        case "Victoria":
-            // Stamp duty calculation for Victoria
-            // if (subtotal <= 250000) {
-            //     stampDuty = 0.01 * subtotal;
-            // } else if (subtotal <= 960000) {
-            //     stampDuty = (0.02 * (subtotal - 250000)) + 2500;
-            // } else if (subtotal <= 2000000) {
-            //     stampDuty = (0.03 * (subtotal - 960000)) + 28700;
-            // } else if (subtotal <= 3000000) {
-            //     stampDuty = (0.04 * (subtotal - 2000000)) + 89900;
-            // } else {
-            //     stampDuty = (0.05 * (subtotal - 3000000)) + 169900;
-            // }
-            // total = subtotal + stampDuty;
+        case "QLD":
+            stampDuty = propertyPrice + 4
             break;
-        case "Western Australia":
-            // taxRate = 0.06;
-            stampDuty = 80; // WA has a $20 discount
-            // total = subtotal + (subtotal * taxRate);
+        case "SA":
+            stampDuty = propertyPrice + 5
+            break;
+        case "TAS":
+            stampDuty = propertyPrice + 6
+            break;
+        case "VIC":
+            if (propertyPrice <= 25000) {
+                stampDuty = propertyPrice * 0.014
+            } else if (propertyPrice <= 130000) {
+                stampDuty = 350 + 0.024 * (propertyPrice - 25000)
+            } else if (propertyPrice <= 440000) {
+                stampDuty = 2870 + 0.05 * (propertyPrice - 130000)
+            } else if (propertyPrice <= 550000) {
+                stampDuty = 18370 + 0.06 * (propertyPrice - 440000)
+            } else if (propertyPrice <= 960000) {
+                stampDuty = 28070 + 0.06 * (propertyPrice - 550000)
+            } else {
+                stampDuty = 0.055 * propertyPrice;
+            }
+            break;
+        case "WA":
+            stampDuty = propertyPrice + 8
+            break;
+        case "ACT":
+            stampDuty = propertyPrice + 9
             break;
         default:
-            stampDuty = 2;
+            stampDuty = 0;
             break;
     }
 
-    // const stampDutyResult = stampDuty
-    stampDuty.textContent = stampDuty.toFixed(2);
-    console.log(stampDuty)
+    // Output the Stamp Duty result in the HTML
+    const stampDutyResultElement = document.getElementById("stampDutyResult");
+    stampDutyResultElement.innerHTML = `$${stampDuty.toFixed(2)}`;
 }
-
-
-
-
-
-
 
 
 
