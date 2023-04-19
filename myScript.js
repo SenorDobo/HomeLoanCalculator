1d2a22892711b60db1e7efcb758cae4554d19639
Adam Dobson-White <adam@onetwo.com.au>
2023-04-19 16:00:21 +1000

    start of land transfer fee calculations. fun times

--- myScript.js
+++ myScript.js
@@ -299,48 +299,33 @@ function landTransferFeeCalculator() {
 //  This function calculates the stamp duty for an owner occupied property in ACT
 //  Function is called from stampDutyCalculator() and is passed the propertyPrice variable when called
 
-function stampDutyCalculatorACT(propertyPrice) {
+function landTransferFeeACT(propertyPrice) {
     //  if propertyPrice is less than or equal to $260,000 then stamp duty calculation is 0.6 x (propertyPrice / 100)
-    if (propertyPrice <= 260000) {
-        stampDuty = 0.6 * (propertyPrice / 100)
-    } else if (propertyPrice <= 300000) {
-        stampDuty = 1560 + (2.2 * ((propertyPrice - 260000) / 100))
-    } else if (propertyPrice <= 500000) {
-        stampDuty = 2440 + (3.4 * ((propertyPrice - 300000) / 100))
-    } else if (propertyPrice <= 750000) {
-        stampDuty = 9240 + (4.32 * ((propertyPrice - 500000) / 100))
-    } else if (propertyPrice <= 1000000) {
-        stampDuty = 20040 + (5.9 * ((propertyPrice - 750000) / 100))
-    } else if (propertyPrice <= 1455000) {
-        stampDuty = 34790 + (5.5 * ((propertyPrice - 1000000) / 100))
-    } else {
-        stampDuty = 4.54 * ((propertyPrice - 3268000) / 100);
-    }
-    //  Returns the stampDuty value to the function that initiated this function
-    return stampDuty
+    // if (propertyPrice <= 260000) {
+    //     stampDuty = 0.6 * (propertyPrice / 100)
+    // } else if (propertyPrice <= 300000) {
+    //     stampDuty = 1560 + (2.2 * ((propertyPrice - 260000) / 100))
+    // } else if (propertyPrice <= 500000) {
+    //     stampDuty = 2440 + (3.4 * ((propertyPrice - 300000) / 100))
+    // } else if (propertyPrice <= 750000) {
+    //     stampDuty = 9240 + (4.32 * ((propertyPrice - 500000) / 100))
+    // } else if (propertyPrice <= 1000000) {
+    //     stampDuty = 20040 + (5.9 * ((propertyPrice - 750000) / 100))
+    // } else if (propertyPrice <= 1455000) {
+    //     stampDuty = 34790 + (5.5 * ((propertyPrice - 1000000) / 100))
+    // } else {
+    //     stampDuty = 4.54 * ((propertyPrice - 3268000) / 100);
+    // }
+    // //  Returns the stampDuty value to the function that initiated this function
+    // return stampDuty
 }
 
 // ****************
 
 //  This function calculates the stamp duty for an owner occupied property in NSW
 
-function stampDutyCalculatorNSW(propertyPrice) {
-    if (propertyPrice <= 15000) {
-        stampDuty = 1.25 * (propertyPrice / 100)
-    } else if (propertyPrice <= 32000) {
-        stampDuty = 187 + (1.50 * ((propertyPrice - 15000) / 100))
-    } else if (propertyPrice <= 87000) {
-        stampDuty = 442 + (1.75 * ((propertyPrice - 32000) / 100))
-    } else if (propertyPrice <= 327000) {
-        stampDuty = 1405 + (3.5 * ((propertyPrice - 87000) / 100))
-    } else if (propertyPrice <= 1089000) {
-        stampDuty = 9805 + (4.5 * ((propertyPrice - 327000) / 100))
-    } else if (propertyPrice <= 3268000) {
-        stampDuty = 44095 + (5.5 * ((propertyPrice - 1089000) / 100))
-    } else {
-        stampDuty = 163940 + (7.0 * ((propertyPrice - 3268000) / 100));
-    }
-    return stampDuty
+function landTransferFeeNSW(propertyPrice) {
+
 }
 
 // ****************
@@ -349,17 +334,8 @@ function stampDutyCalculatorNSW(propertyPrice) {
 
 //  Source: https://www.homeloanexperts.com.au/mortgage-calculators/stamp-duty-calculator-nt/
 
-function stampDutyCalculatorNT(propertyPrice) {
-    if (propertyPrice <= 525000) {
-        stampDuty = (0.06571441 * Math.pow((propertyPrice / 1000), 2)) + (15 * (propertyPrice / 1000))
-    } else if (propertyPrice <= 2999999) {
-        stampDuty = 0.0495 * propertyPrice
-    } else if (propertyPrice <= 4999999) {
-        stampDuty = 0.0575 * propertyPrice
-    } else {
-        stampDuty = 0.0595 * propertyPrice;
-    }
-    return stampDuty
+function landTransferFeeNT(propertyPrice) {
+
 }
 
 // ****************
@@ -368,17 +344,8 @@ function stampDutyCalculatorNT(propertyPrice) {
 
 //  Source: https://conveyancing.com.au/tools/stamp-duty-calculator-vic
 
-function stampDutyCalculatorQLD(propertyPrice) {
-    if (propertyPrice <= 350000) {
-        stampDuty = 1.0 * (propertyPrice / 100)
-    } else if (propertyPrice <= 540000) {
-        stampDuty = 3500 + (3.5 * ((propertyPrice - 350000) / 100))
-    } else if (propertyPrice <= 1000000) {
-        stampDuty = 10150 + (4.5 * ((propertyPrice - 540000) / 100))
-    } else {
-        stampDuty = 30850 + (5.75 * ((propertyPrice - 1000000) / 100));
-    }
-    return stampDuty
+function landTransferFeeQLD(propertyPrice) {
+
 }
 
 // ****************
@@ -387,178 +354,84 @@ function stampDutyCalculatorQLD(propertyPrice) {
 
 //  Source: https://conveyancing.com.au/tools/stamp-duty-calculator-vic
 
-function stampDutyCalculatorSA(propertyPrice) {
-    if (propertyPrice <= 12000) {
-        stampDuty = propertyPrice / 100
-    } else if (propertyPrice <= 30000) {
-        stampDuty = 120 + (2.0 * ((propertyPrice - 12000) / 100))
-    } else if (propertyPrice <= 50000) {
-        stampDuty = 480 + (3.0 * ((propertyPrice - 30000) / 100))
-    } else if (propertyPrice <= 100000) {
-        stampDuty = 1080 + (3.5 * ((propertyPrice - 50000) / 100))
-    } else if (propertyPrice <= 200000) {
-        stampDuty = 2830 + (4.0 * ((propertyPrice - 100000) / 100))
-    } else if (propertyPrice <= 250000) {
-        stampDuty = 6830 + (4.25 * ((propertyPrice - 200000) / 100))
-    } else if (propertyPrice <= 300000) {
-        stampDuty = 8955 + (4.75 * ((propertyPrice - 250000) / 100))
-    } else if (propertyPrice <= 500000) {
-        stampDuty = 11330 + (5.0 * ((propertyPrice - 300000) / 100))
-    } else {
-        stampDuty = 21330 + (5.5 * ((propertyPrice - 500000) / 100));
-    }
-    return stampDuty
+function landTransferFeeSA(propertyPrice) {
+
 }
 
 // ****************
 
 //  This function calculates the stamp duty for an owner occupied property in TAS
 
-function stampDutyCalculatorTAS(propertyPrice) {
-    if (propertyPrice <= 3000) {
-        stampDuty = 50
-    } else if (propertyPrice <= 25000) {
-        stampDuty = 50 + (1.75 * ((propertyPrice - 3000) / 100))
-    } else if (propertyPrice <= 75000) {
-        stampDuty = 435 + (2.25 * ((propertyPrice - 25000) / 100))
-    } else if (propertyPrice <= 200000) {
-        stampDuty = 1560 + (3.5 * ((propertyPrice - 75000) / 100))
-    } else if (propertyPrice <= 375000) {
-        stampDuty = 5935 + (4.0 * ((propertyPrice - 200000) / 100))
-    } else if (propertyPrice <= 725000) {
-        stampDuty = 12935 + (4.25 * ((propertyPrice - 375000) / 100))
-    } else {
-        stampDuty = 27810 + (4.5 * ((propertyPrice - 500000) / 100));
-    }
-    return stampDuty
+function landTransferFeeTAS(propertyPrice) {
+
 }
 
 // ****************
 
 //  This function calculates the stamp duty for an owner occupied property in VIC
 
-function stampDutyCalculatorVIC(propertyPrice) {
-    if (propertyPrice <= 25000) {
-        stampDuty = 0.014 * propertyPrice
-    } else if (propertyPrice <= 130000) {
-        stampDuty = 350 + 0.024 * (propertyPrice - 25000)
-    } else if (propertyPrice <= 440000) {
-        stampDuty = 2870 + 0.05 * (propertyPrice - 130000)
-    } else if (propertyPrice <= 550000) {
-        stampDuty = 18370 + 0.06 * (propertyPrice - 440000)
-    } else if (propertyPrice <= 960000) {
-        stampDuty = 28070 + 0.06 * (propertyPrice - 550000)
-    } else {
-        stampDuty = 0.055 * propertyPrice;
-    }
-    return stampDuty
+function landTransferFeeVIC(propertyPrice) {
+
 }
 
 // ****************
 
-//  This function calculates the stamp duty for an owner occupied property in WA
-
-function stampDutyCalculatorWA(propertyPrice) {
-
-    if (propertyPrice <= 80000) {
-        stampDuty = 1.9 * (propertyPrice / 100)
-    } else if (propertyPrice <= 100000) {
-        stampDuty = 1520 + (2.85 * ((propertyPrice - 80000) / 100))
-    } else if (propertyPrice <= 250000) {
-        stampDuty = 2090 + (3.8 * ((propertyPrice - 100000) / 100))
-    } else if (propertyPrice <= 500000) {
-        stampDuty = 7790 + (4.75 * ((propertyPrice - 250000) / 100))
-    } else {
-        stampDuty = 19665 + (5.15 * ((propertyPrice - 500000) / 100));
-    }
-    return stampDuty
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//  This function calculates the land transfer fee for an owner occupied property in WA
 //  Source: https://conveyancing.com.au/tools/stamp-duty-calculator-vic
 
-//  WA
+
+function landTransferFeeWA(propertyPrice) {
 
 
-// Land Transfer Fee from 1 July 2022:
+    if (propertyPrice <= 85000) {
+        landTransferFee = 187.60
+    } else if (propertyPrice <= 120000) {
+        landTransferFee = 197.60
+    } else if (propertyPrice <= 200000) {
+        landTransferFee = 217.60
+    } else if (propertyPrice <= 300000) {
+        landTransferFee = 237.60
+    } else if (propertyPrice <= 400000) {
+        landTransferFee = 257.60
+    } else if (propertyPrice <= 500000) {
+        landTransferFee = 277.60
+    } else if (propertyPrice <= 600000) {
+        landTransferFee = 297.60
+    } else if (propertyPrice <= 700000) {
+        landTransferFee = 317.60
+    } else if (propertyPrice <= 800000) {
+        landTransferFee = 337.60
+    } else if (propertyPrice <= 900000) {
+        landTransferFee = 357.60
+    } else if (propertyPrice <= 1000000) {
+        landTransferFee = 377.60
+    } else if (propertyPrice <= 1100000) {
+        landTransferFee = 397.60
+    } else if (propertyPrice <= 1200000) {
+        landTransferFee = 417.60
+    } else if (propertyPrice <= 1300000) {
+        landTransferFee = 437.60
+    } else if (propertyPrice <= 1400000) {
+        landTransferFee = 457.60
+    } else if (propertyPrice <= 1500000) {
+        landTransferFee = 477.60
+    } else if (propertyPrice <= 1600000) {
+        landTransferFee = 497.60
+    } else if (propertyPrice <= 1700000) {
+        landTransferFee = 517.60
+    } else if (propertyPrice <= 1800000) {
+        landTransferFee = 537.60
+    } else if (propertyPrice <= 1900000) {
+        landTransferFee = 557.60
+    } else if (propertyPrice <= 2000000) {
+        landTransferFee = 577.60
+    } else {
+        landTransferFee = 577.60 + (20 * (propertyPrice / 100));
 
-// Registration fees for Transfer
-// Up to $85,000	$187.60
-// $85,001 - $120,000	$197.60
-// $120,001 - $200,000	$217.60
-// $200,001 - $300,000	$237.60
-// $300,001 - $400,000	$257.60
-// $400,001 - $500,000	$277.60
-// $500,001 - $600,000	$297.60
-// $600,001 - $700,000	$317.60
-// $700,001 - $800,000	$337.60
-// $800,001 - $900,000	$357.60
-// $900,001 - $1,000,000	$377.60
-// $1,000,001 - $1,100,000	$397.60
-// $1,100,001 - $1,200,000	$417.60
-// $1,200,001 - $1,300,000	$437.60
-// $1,300,001 - $1,400,000	$457.60
-// $1,400,001 - $1,500,000	$477.60
-// $1,500,001 - $1,600,000	$497.60
-// $1,600,001 - $1,700,000	$517.60
-// $1,700,001 - $1,800,000	$537.60
-// $1,800,001 - $1,900,000	$557.60
-// $1,900,001 - $2,000,000	$577.60
-// Over $2,000,000 - $577.60 plus $20 for every $100,000 or part thereof.
-
-
-
-
-
-
-
+    }
+    //  Returns the landTransferFee value to the function that initiated this function
+    return landTransferFee
+}
 
 
 
